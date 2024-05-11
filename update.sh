4bfd21e46774bee9fad0e128a6e4e1c9a25db88b
Martin Kielhorn <wolpumba@gmail.com>
2024-05-11 09:52:59 +0200

    change update commandsc

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
+sbcl --eval '(ql:update-dist "quicklisp")'
+sbcl --eval '(ql:update-client)'
+# i had an issue with swank 2.28 in quicklisp and 2.29.1 in gentoo
+# i decided to remove the version coming with gentoo
+# sbcl --eval '(ql:quickload "quicklisp-slime-helper")'
+# sbcl --eval '(ql:uninstall "swank")'
+# sudo emerge -c app-emacs/slime
 sbcl --eval "(map nil 'ql-dist:clean (ql-dist:all-dists))"
 apt-get update
 apt-get upgrade
